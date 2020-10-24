59d878aa85edf06bd02f1aba637e9b2e4c348fcc
Jimmy <jimmypannu_18@yahoo.co.in>
2020-10-24 20:36:09 +1100

    new updates

--- main.rb
+++ main.rb
@@ -31,7 +31,7 @@ end
 
 
 get '/' do
-
+  redirect '/login' unless  logged_in?
   players = run_sql("SELECT * FROM players")
 
   erb :index, locals: {
@@ -41,12 +41,13 @@ get '/' do
 end
 
 get '/players/new' do
-
+  redirect '/login' unless  logged_in?
   erb :new
 
 end
 
 get '/players/:id' do
+  redirect '/login' unless  logged_in?
 
   sql = "SELECT * FROM players WHERE id = #{ params['id'] };"
 
@@ -66,7 +67,7 @@ post '/players' do
 end
 
 delete '/players/:id' do
-
+  redirect '/login' unless  logged_in?
   sql = "DELETE FROM players WHERE id = #{ params['id'] };"
 
   results = run_sql(sql)
@@ -76,7 +77,7 @@ delete '/players/:id' do
 end
 
 get '/players/:id/edit' do
-  
+  redirect '/login' unless  logged_in?
   sql = "SELECT * FROM players WHERE id = #{params['id']};"
 
   results = run_sql(sql)
@@ -87,7 +88,7 @@ get '/players/:id/edit' do
 end
 
 patch '/players/:id' do
-
+  redirect '/login' unless  logged_in?
   sql = "UPDATE players SET name = '#{params['name']}', image_url = '#{params['image_url']}', age = '#{params['age']}', team = '#{params['team']}', playing_role = '#{params['playing_role']}' WHERE id = '#{params['id']}';"
 
   results = run_sql(sql)
@@ -104,15 +105,7 @@ end
 
 post '/login' do
 
-  # params['email'] = 'jim@ga.com'
-  # params['password'] = 'cricket'
-  # results = run_sql("select * from users where email = '#{params['email']}';")
-  # redirect '/'
-  # erb :login
-  # user = results[0]
   user = find_user_by_email(params['email'])
-  # user.to_a.to_s
-
   if BCrypt::Password.new(user['password_digest']) == params['password']
     session[:user_id] = user['id']
     redirect "/"
